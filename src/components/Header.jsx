e6aed2d80a7ffa8341328b38108608099f78fab8
Lawrence Caudle <lawrencecaudle870@gmail.com>
2023-03-03 16:00:18 -0500

    making changes to header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,49 +1,36 @@
 import mobileHeroImage from "../assets/mobile/image-hero.jpg";
 import mobileHeroImage2 from "../assets/mobile/image-hero@2x.jpg";
-import tabletHeroImage from "../assets/tablet/image-hero.jpg";
-import tabletHeroImage2 from "../assets/tablet/image-hero@2x.jpg";
-import desktopHeroImage from "../assets/desktop/image-hero.jpg";
-import desktopHeroImage2 from "../assets/desktop/image-hero@2x.jpg";
 import Button from "./Button";
 import classes from "./Header.module.css";
 
 const Header = (props) => {
-    const { homePageLocation } = props;
+  const { homePageLocation } = props;
 
-    return (
-        <header className={classes.header}>
-            <picture className={classes.headerHeroPicOne}>
-                <source
-                    media="(min-width: 1024px)"
-                    srcSet={`${desktopHeroImage}, ${desktopHeroImage2} 2x`}
-                />
-                <source
-                    media="(min-width: 768px)"
-                    srcSet={`${tabletHeroImage}, ${tabletHeroImage2} 2x`}
-                />
-                <img
-                    className={classes.headerHeroImage}
-                    srcSet={`${mobileHeroImage}, ${mobileHeroImage2} 2x`}
-                    src={mobileHeroImage}
-                    alt="A man and a woman looking at a painting of an elderly woman."
-                />
-            </picture>
+  return (
+    <header className={classes.header}>
+      <img
+        className={classes.headerHeroImage}
+        src={mobileHeroImage}
+        alt="A man and a woman looking at a painting of an elderly woman."
+      />
 
-            <div className={classes.headerContainer}>
-                <h1 className={classes.headerTitle}>
-                    Modern <br /> Art Gallery
-                </h1>
-                <div className={classes.headerTextContainer}>
-                    <p className={classes.headerText}>
-                        The arts in the collection of the Modern Art Gallery all
-                        started from a spark of inspiration. Will these pieces
-                        inspire you? Visit us and find out.
-                    </p>
-                    <Button homePageLocation={homePageLocation} />
-                </div>
-            </div>
-        </header>
-    );
+      <div className={classes.headerContainer}>
+        <div className={classes.headerBox}>
+          <h1 className={classes.headerTitle}>
+            Modern <br /> Art Gallery
+          </h1>
+          <div className={classes.headerTextContainer}>
+            <p className={classes.headerText}>
+              The arts in the collection of the Modern Art Gallery all started
+              from a spark of inspiration. Will these pieces inspire you? Visit
+              us and find out.
+            </p>
+            <Button homePageLocation={homePageLocation} />
+          </div>
+        </div>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
