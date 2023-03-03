527fce6fddc958421b4aad67fe663a4a64cc97d0
Lawrence Caudle <lawrencecaudle870@gmail.com>
2023-03-03 18:28:01 -0500

    worked on map in Map

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -4,29 +4,29 @@ import { myIcon } from "../mappings/icon";
 import classes from "./Map.module.css";
 
 const Map = (props) => {
-    const { locationPageLocation } = props;
+  const { locationPageLocation } = props;
 
-    return (
-        <header>
-            <MapContainer
-                className={classes.leafletContainer}
-                center={[41.48144, -71.31035]}
-                zoom={15}
-                scrollWheelZoom={false}
-            >
-                <TileLayer
-                    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-                />
-                <Marker position={[41.48144, -71.31035]} icon={myIcon}>
-                    <Popup>
-                        A pretty CSS3 popup. <br /> Easily customizable.
-                    </Popup>
-                </Marker>
-            </MapContainer>
-            <Button locationPageLocation={locationPageLocation} />
-        </header>
-    );
+  return (
+    <header>
+      <MapContainer
+        className={classes.leafletContainer}
+        center={[41.48144, -71.31035]}
+        zoom={15}
+        scrollWheelZoom={false}
+        style={{ filter: "grayscale(100)" }}>
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <Marker position={[41.48144, -71.31035]} icon={myIcon}>
+          <Popup>
+            A pretty CSS3 popup. <br /> Easily customizable.
+          </Popup>
+        </Marker>
+      </MapContainer>
+      <Button locationPageLocation={locationPageLocation} />
+    </header>
+  );
 };
 
 export default Map;
