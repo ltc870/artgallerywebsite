345aad008528614305810e0dfcad58b9848d02fe
Lawrence Caudle <lawrencecaudle870@gmail.com>
2023-03-04 13:44:55 -0500

    added classNames for conditional rendering

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,67 +2,83 @@ import { Link } from "react-router-dom";
 import classes from "./Footer.module.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
-    faFacebookSquare,
-    faTwitter,
-    faInstagram,
+  faFacebookSquare,
+  faTwitter,
+  faInstagram,
 } from "@fortawesome/free-brands-svg-icons";
 import darkLogo from "../assets/logo-dark.svg";
 import lightLogo from "../assets/logo-light.svg";
 
 const Footer = (props) => {
-    const { homePageLocation } = props;
+  const { homePageLocation } = props;
 
-    return (
-        <footer
-            className={
-                homePageLocation ? classes.footer : classes.locationFooter
-            }
+  return (
+    <footer
+      className={homePageLocation ? classes.footer : classes.locationFooter}
+    >
+      <div
+        className={
+          homePageLocation
+            ? classes.footerContainer
+            : classes.locationFooterContainer
+        }
+      >
+        <img
+          className={homePageLocation ? classes.lightLogo : classes.darkLogo}
+          src={homePageLocation ? lightLogo : darkLogo}
+          alt="Footer logo"
+        />
+        <p
+          className={
+            homePageLocation ? classes.footerText : classes.locationFooterText
+          }
         >
-            <div className={classes.footerContainer}>
-                <img className={homePageLocation ? classes.lightLogo : classes.darkLogo} 
-                     src={homePageLocation ? lightLogo : darkLogo} alt="Footer logo" />
-                <p className={homePageLocation ? classes.footerText : classes.locationFooterText}>
-                    The Modern Art Gallery is free to all visitors and open
-                    seven days a week from 8am to 9pm. Find us at 99 King
-                    Street, Newport, USA.
-                </p>
-                <div className={classes.footerSocialMedia}>
-                    <Link>
-                        <FontAwesomeIcon
-                            icon={faFacebookSquare}
-                            className={
-                                homePageLocation
-                                    ? classes.socialMedia
-                                    : classes.socialMediaLocation
-                            }
-                        />
-                    </Link>
+          The Modern Art Gallery is free to all visitors and open seven days a
+          week from 8am to 9pm. Find us at 99 King Street, Newport, USA.
+        </p>
+        <div
+          className={
+            homePageLocation
+              ? classes.footerSocialMedia
+              : classes.locationFooterSocialMedia
+          }
+        >
+          <Link>
+            <FontAwesomeIcon
+              icon={faFacebookSquare}
+              className={
+                homePageLocation
+                  ? classes.socialMedia
+                  : classes.socialMediaLocation
+              }
+            />
+          </Link>
 
-                    <Link>
-                        <FontAwesomeIcon
-                            icon={faInstagram}
-                            className={
-                                homePageLocation
-                                    ? classes.socialMedia
-                                    : classes.socialMediaLocation
-                            }
-                        />
-                    </Link>
+          <Link>
+            <FontAwesomeIcon
+              icon={faInstagram}
+              className={
+                homePageLocation
+                  ? classes.socialMedia
+                  : classes.socialMediaLocation
+              }
+            />
+          </Link>
 
-                    <Link>
-                        <FontAwesomeIcon
-                            icon={faTwitter}
-                            className={
-                                homePageLocation
-                                    ? classes.socialMedia
-                                    : classes.socialMediaLocation
-                            }
-                        />
-                    </Link>
-                </div>
-            </div>
-        </footer>
-    );
+          <Link>
+            <FontAwesomeIcon
+              icon={faTwitter}
+              className={
+                homePageLocation
+                  ? classes.socialMedia
+                  : classes.socialMediaLocation
+              }
+            />
+          </Link>
+        </div>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
