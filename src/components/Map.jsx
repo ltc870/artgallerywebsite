a69e2613d49bf5bee3b4eb561e5609bb24aaf25b
Lawrence Caudle <lawrencecaudle870@gmail.com>
2023-03-07 22:57:36 -0500

    attempted to correct the map

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,5 +1,11 @@
 import Button from "./Button";
-import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import {
+  MapContainer,
+  TileLayer,
+  Marker,
+  Popup,
+  ZoomControl,
+} from "react-leaflet";
 import { myIcon } from "../mappings/icon";
 import classes from "./Map.module.css";
 
@@ -13,18 +19,23 @@ const Map = (props) => {
         center={[41.48144, -71.31035]}
         zoom={15}
         scrollWheelZoom={false}
-        style={{ filter: "grayscale(100)" }}
+        style={{ filter: "grayscale(100%)" }}
         zoomControl={false}
       >
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
-        <Marker position={[41.48144, -71.31035]} icon={myIcon}>
+        <Marker
+          position={[41.48413, -71.31035]}
+          icon={myIcon}
+          zIndexOffset={500}
+        >
           <Popup>
             A pretty CSS3 popup. <br /> Easily customizable.
           </Popup>
         </Marker>
+        <ZoomControl position="bottomright" />
       </MapContainer>
       <Button locationPageLocation={locationPageLocation} />
     </header>
--- src/mappings/icon.js
+++ src/mappings/icon.js
@@ -1,9 +1,9 @@
 import icon from "../assets/icon-location.svg";
 
 export const myIcon = L.icon({
-    iconUrl: icon,
-    iconSize: [40, 55],
-    iconAnchor: [22, 94],
-    popupAnchor: [-3, -76],
-    shadowAnchor: [22, 94],
+  iconUrl: icon,
+  iconSize: [40, 55],
+  iconAnchor: [22, 94],
+  popupAnchor: [-3, -76],
+  shadowAnchor: [22, 94],
 });
