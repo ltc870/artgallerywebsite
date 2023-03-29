bda6aaf88fafede6f69cbcfb598e67c387e96864
Lawrence Caudle <lawrencecaudle870@gmail.com>
2023-03-29 12:52:14 -0400

    added error page

--- src/App.jsx
+++ src/App.jsx
@@ -9,12 +9,14 @@ import Root from "./pages/RootPage";
 import HomePage from "./pages/HomePage";
 import LocationPage from "./pages/LocationPage";
 import "./App.css";
+import NoMatch from "./pages/NoMatch";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<Root />}>
       <Route element={<HomePage />} index />
       <Route path="/location" element={<LocationPage />} />
+      <Route path="*" errorElement={<NoMatch />} />
     </Route>
   )
 );
---
+++ src/pages/NoMatch.jsx
@@ -0,0 +1,5 @@
+const NoMatch = () => {
+  return <h1>No patch matching that URL</h1>;
+};
+
+export default NoMatch;
