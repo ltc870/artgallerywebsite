4ea51edbc4a625feaf2f31810d30e62a9aee72a1
Lawrence Caudle <lawrencecaudle870@gmail.com>
2023-03-28 21:40:59 -0400

    saved file

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import {
-    createBrowserRouter,
-    createRoutesFromElements,
-    Route,
-    RouterProvider,
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
 } from "react-router-dom";
 import Root from "./pages/RootPage";
 import HomePage from "./pages/HomePage";
@@ -11,16 +11,16 @@ import LocationPage from "./pages/LocationPage";
 import "./App.css";
 
 const router = createBrowserRouter(
-    createRoutesFromElements(
-        <Route path="/" element={<Root />}>
-            <Route element={<HomePage />} index />
-            <Route path="/location" element={<LocationPage />} />
-        </Route>
-    )
+  createRoutesFromElements(
+    <Route path="/" element={<Root />}>
+      <Route element={<HomePage />} index />
+      <Route path="/location" element={<LocationPage />} />
+    </Route>
+  )
 );
 
 function App() {
-    return <RouterProvider router={router} />;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
