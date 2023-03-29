588aa9c1668b4619d8df3320f739ea95cdd13db0
Lawrence Caudle <lawrencecaudle870@gmail.com>
2023-03-28 22:05:54 -0400

    edited RootPage

--- src/pages/RootPage.jsx
+++ src/pages/RootPage.jsx
@@ -1,11 +1,11 @@
 import { Outlet } from "react-router-dom";
 
 const Root = () => {
-    return (
-        <div>
-            <Outlet />
-        </div>
-    );
+  return (
+    <div>
+      <Outlet />
+    </div>
+  );
 };
 
 export default Root;
